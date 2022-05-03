a91c3cf9978b8b6d7cebb715de8735f6410968c2
Oscar <oscarskapee@gmail.com>
2022-05-02 21:01:51 -0500

    Practica 2 terminada agregando api

---
+++ test/ExplorerController.test.js
@@ -0,0 +1,15 @@
+const ExplorerController = require("../lib/controllers/ExplorerController");
+
+describe("Test para clase ExplorerController", () => {
+    test("Prueba 1", () => { 
+        //Aqui se coloca el codigo a testear
+        const filterMission = ExplorerController.getExplorersByMission("node");
+        expect(filterMission[0].mission).toBe("node");
+
+        const numExplorerPorMision = ExplorerController.getExplorersAmonutByMission("node");
+        expect(numExplorerPorMision).toBe(10);
+
+        const users = ExplorerController.getExplorersUsernamesByMission("node");
+        expect(users[6]).toBe("Woopa12");
+    });
+});
