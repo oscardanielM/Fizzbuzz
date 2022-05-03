05d02dde0c14145f4f07af45976b666381587bf1
Oscar <oscarskapee@gmail.com>
2022-05-02 21:30:21 -0500

    Practica 3 terminada agregando prubas de getApplyValidationInNumber

--- test/ExplorerController.test.js
+++ test/ExplorerController.test.js
@@ -14,14 +14,16 @@ describe("Test para clase ExplorerController", () => {
     });
 
     test("Prueba 2", () => { 
-        //Aqui se coloca el codigo a testear
-        const filterMission = ExplorerController.getExplorersByMission("node");
-        expect(filterMission[0].mission).toBe("node");
+        const trinck1 = ExplorerController.getApplyValidationInNumber(1).trick; // {name: "Explorer1", score: 1, trick: 1} 
+        expect(trinck1).toBe(1);
 
-        const numExplorerPorMision = ExplorerController.getExplorersAmonutByMission("node");
-        expect(numExplorerPorMision).toBe(10);
+        const trinck3 = ExplorerController.getApplyValidationInNumber(3).trick; // {name: "Explorer3", score: 3, trick: "FIZZ"}
+        expect(trinck3).toBe("FIZZ");
 
-        const users = ExplorerController.getExplorersUsernamesByMission("node");
-        expect(users[6]).toBe("Woopa12");
+        const trinck5 = ExplorerController.getApplyValidationInNumber(5).trick; // {name: "Explorer5", score: 5, trick: "BUZZ"}
+        expect(trinck5).toBe("BUZZ");
+
+        const trinck3y5 = ExplorerController.getApplyValidationInNumber(15).trick; // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
+        expect(trinck3y5).toBe("FIZZBUZZ");
     });
 });
