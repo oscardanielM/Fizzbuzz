926d8d3f58bf0fab985432844a56521a5c336770
Oscar <oscarskapee@gmail.com>
2022-05-02 21:26:24 -0500

    Practica 3 terminada agregando api FizzbuzzService

--- test/ExplorerController.test.js
+++ test/ExplorerController.test.js
@@ -12,4 +12,16 @@ describe("Test para clase ExplorerController", () => {
         const users = ExplorerController.getExplorersUsernamesByMission("node");
         expect(users[6]).toBe("Woopa12");
     });
+
+    test("Prueba 2", () => { 
+        //Aqui se coloca el codigo a testear
+        const filterMission = ExplorerController.getExplorersByMission("node");
+        expect(filterMission[0].mission).toBe("node");
+
+        const numExplorerPorMision = ExplorerController.getExplorersAmonutByMission("node");
+        expect(numExplorerPorMision).toBe(10);
+
+        const users = ExplorerController.getExplorersUsernamesByMission("node");
+        expect(users[6]).toBe("Woopa12");
+    });
 });
